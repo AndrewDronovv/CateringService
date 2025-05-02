2cc383562bb938959715cad555a2393af20fddcc
Andrew <andrewdronofff@gmail.com>
2025-05-02 12:51:48 +0300

    - Добавление Multi-Tenancy в проект.

---
+++ CateringService.Domain/Entities/Tenant.cs
@@ -0,0 +1,10 @@
+﻿using CateringService.Domain.Common;
+
+namespace CateringService.Domain.Entities;
+
+public class Tenant : UlidEntity
+{
+    public string Name { get; set; } = string.Empty;
+    public bool IsActive { get; set; } = true;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+}
