6b730098efd69a3448c9fd291892c240ff0d3353
Andrew <andrewdronofff@gmail.com>
2025-05-31 13:25:47 +0300

    - Переработан сервис GetMenuCategoriesAsync и контроллер GetMenuCategoryBySupplierIdAsync для удаления категории меню. - Доработан метод CreateDishAsync. - Logging и exception зависимости убраны из DishesController, все добавлено в DishService. - Изменен сервис и контроллер для Address.

--- CateringService/Controllers/AddressesController.cs
+++ CateringService/Controllers/AddressesController.cs
@@ -3,37 +3,36 @@ using CateringService.Application.DataTransferObjects.Requests;
 using CateringService.Application.DataTransferObjects.Responses;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CateringService.Controllers
+namespace CateringService.Controllers;
+
+[ApiController]
+public class AddressesController : ControllerBase
 {
-    [ApiController]
-    public class AddressesController : ControllerBase
+    private readonly IAddressService _addressService;
+    public AddressesController(IAddressService addressService)
     {
-        private readonly IAddressService _addressService;
-        public AddressesController(IAddressService addressService)
-        {
-            _addressService = addressService;
-        }
+        _addressService = addressService;
+    }
 
-        [HttpPost(ApiEndPoints.Addresses.Create)]
-        [ProducesResponseType(typeof(AddressViewModel), StatusCodes.Status201Created)]
-        [ProducesResponseType(typeof(object), StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(typeof(object), StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<AddressViewModel>> CreateAddressAsync([FromBody] AddAddressRequest request)
-        {
-            var createdAddress = await _addressService.CreateAddressAsync(request, Ulid.NewUlid());
+    [HttpPost(ApiEndPoints.Addresses.Create)]
+    [ProducesResponseType(typeof(AddressViewModel), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(object), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(object), StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<AddressViewModel>> CreateAddressAsync([FromBody] AddAddressRequest request)
+    {
+        var createdAddress = await _addressService.CreateAddressAsync(request, Ulid.NewUlid());
 
-            return CreatedAtRoute("GetAddressById", new { addressId = createdAddress.Id }, createdAddress);
-        }
+        return CreatedAtRoute("GetAddressById", new { addressId = createdAddress.Id }, createdAddress);
+    }
 
-        [HttpGet(ApiEndPoints.Addresses.Get, Name = "GetAddressById")]
-        [ProducesResponseType(typeof(AddressViewModel), StatusCodes.Status200OK)]
-        [ProducesResponseType(typeof(object), StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(typeof(object), StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<AddressViewModel>> GetAddressAsync(Ulid addressId)
-        {
-            var address = await _addressService.GetByIdAsync(addressId);
+    [HttpGet(ApiEndPoints.Addresses.Get, Name = "GetAddressById")]
+    [ProducesResponseType(typeof(AddressViewModel), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(object), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(object), StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<AddressViewModel>> GetAddressAsync(Ulid addressId)
+    {
+        var address = await _addressService.GetByIdAsync(addressId);
 
-            return Ok(address);
-        }
+        return Ok(address);
     }
 }
