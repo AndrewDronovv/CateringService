8b2f324aa0ef607e1de3c0ddd1dcc5053ff8f9e7
Andrew <andrewdronofff@gmail.com>
2025-06-01 22:46:08 +0300

    - Добавлен метод SearchByZipAsync в AddressRepository.

--- CateringService.Persistence/Repositories/AddressRepository.cs
+++ CateringService.Persistence/Repositories/AddressRepository.cs
@@ -1,5 +1,6 @@
 ﻿using CateringService.Domain.Entities.Approved;
 using CateringService.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace CateringService.Persistence.Repositories;
 
@@ -7,5 +8,19 @@ public class AddressRepository : GenericRepository<Address, Ulid>, IAddressRepos
 {
     public AddressRepository(AppDbContext context) : base(context)
     {
+    }
+
+    public async Task<IEnumerable<Address>> SearchByZipAsync(Ulid? tenantId, string zip)
+    {
+        var query = _context.Addresses.AsNoTracking();
+
+        if (tenantId.HasValue)
+        {
+            query = query.Where(a => a.TenantId == tenantId.Value);
+        }
+
+        query = query.Where(a => a.Zip == zip);
+
+        return await query.OrderByDescending(a => a.CreatedAt).ToListAsync();
     }
 }
