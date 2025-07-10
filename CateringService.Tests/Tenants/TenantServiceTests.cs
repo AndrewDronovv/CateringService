41b9901b9109e2f8f1b5968bba44f00fc4f9701b
Andrew <andrewdronofff@gmail.com>
2025-07-10 23:36:44 +0300

    - Добавлены тесты для TenantService.

---
+++ CateringService.Tests/Tenants/TenantServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using CateringService.Application.Abstractions;
+using CateringService.Application.Services;
+using CateringService.Domain.Repositories;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+
+namespace CateringService.Tests.Tenants;
+
+public sealed class TenantServiceTests
+{
+    private readonly ITenantRepository _tenantRepository;
+    private readonly IUnitOfWorkRepository _unitOfWorkRepository;
+    private readonly IMapper _mapper;
+    private readonly ILogger<TenantService> _logger;
+    private readonly ITenantService _tenantService;
+
+    public TenantServiceTests()
+    {
+        _tenantRepository = Substitute.For<ITenantRepository>();
+        _unitOfWorkRepository = Substitute.For<IUnitOfWorkRepository>();
+        _mapper = Substitute.For<IMapper>();
+        _logger = Substitute.For<ILogger<TenantService>>();
+
+        _tenantService = new TenantService(_tenantRepository, _unitOfWorkRepository, _mapper, _logger);
+    }
+
+    [Fact]
+    public async Task Ctor_WhenTenantRepositoryNull_ShouldThrowArgumentNullException()
+    {
+        var exception = Assert.Throws<ArgumentNullException>(() => new TenantService(null!, _unitOfWorkRepository, _mapper, _logger));
+        Assert.Contains("tenantRepository", exception.Message);
+    }
+}
